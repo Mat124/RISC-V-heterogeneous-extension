a4421a7ad4864c44a21aa63649660edc19c38226
Matthew Knight <mrg.knight1@gmail.com>
2023-01-16 15:57:14 +0000

    add ipi gen

--- bare-metal/interrupt-gen/main.c
+++ bare-metal/interrupt-gen/main.c
@@ -4,6 +4,11 @@
 #include "common.h"
 #include "kprintf.h"
 
+
+#define CLINT_BASE_ADDR 0x02000000
+#define MSIP_PER_HART_OFFSET    0x4
+#define MSIP_BASE_ADDR(hartid)  (CLINT_BASE_ADDR + (hartid * MSIP_PER_HART_OFFSET))
+
 static void usleep(unsigned us) {
     uintptr_t cycles0;
     uintptr_t cycles1;
@@ -24,8 +29,9 @@ int main(void) {
     kprintf("This is hart %d!\n", hart); //not portable as using %d
     usleep(1000000);
 
-    if (hart == 0) {
-        //create interrupt
+    if (hart == 0) { //start other hart
+        uintptr_t msip_addr = MSIP_BASE_ADDR(1); //MSIP for hart 1
+        (*(uint32_t *)(msip_addr)) = 0x1; //create interrupt
     }
     
     return 0;
